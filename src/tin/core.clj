2f6cf3f55da009ac31b2e282ba807a7b36a1fa0a
Derek Thurn <derek@fake.email>
2016-06-15 09:21:50 -0700

    tokens

--- src/tin/core.clj
+++ src/tin/core.clj
@@ -1,7 +1,15 @@
 (ns tin.core
+  (:require [clojure.string :as str])
   (:gen-class))
+
+(defn tokenize [string]
+  (str/split
+   (str/replace (str/replace string "(" " ( ") ")" " ) ")
+   #" "))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println "Hello, World!")
+  (println (tokenize "(+ 1 2)"))
+  )
