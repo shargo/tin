569bc84db4af8ee8f8eec4bca4b048315c407e1e
Derek Thurn <derek@fake.email>
2016-06-17 10:49:26 -0700

    reading

--- src/tin/core.clj
+++ src/tin/core.clj
@@ -7,6 +7,20 @@
    (str/replace (str/replace string "(" " ( ") ")" " ) ")
    #" "))
 
+(defn parse [string]
+  (read-from-tokens (tokenize string)))
+
+(defn read-from-tokens [tokens]
+  (if (zero? (count tokens))
+    (throw (Exception. "Unexpected EOF while reading")))
+  (let [[token & tokens] tokens]
+    (case token
+      "(" (map read-from-tokens (take-while #(not= ")" %1) tokens))
+      ")" (throw (Exception. "Unexpected ) while reading"))
+      (atom token))))
+
+(defn atom)
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
