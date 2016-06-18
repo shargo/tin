f91d1213b9161c14ba940be3ba32a8ebfd012a84
Derek Thurn <derek@fake.email>
2016-06-18 10:50:27 -0700

    parsing

--- src/tin/core.clj
+++ src/tin/core.clj
@@ -3,27 +3,31 @@
   (:gen-class))
 
 (defn tokenize [string]
-  (str/split
-   (str/replace (str/replace string "(" " ( ") ")" " ) ")
-   #" "))
+  (filter #(not= %1 "")
+          (str/split
+           (str/replace
+            (str/replace string "(" " ( ") ")" " ) ")
+           #" ")))
 
-(defn parse [string]
-  (read-from-tokens (tokenize string)))
+(defn atomic [token] {:token token})
 
 (defn read-from-tokens [tokens]
+  ;(println "read-from-tokens>" tokens "<")
   (if (zero? (count tokens))
     (throw (Exception. "Unexpected EOF while reading")))
   (let [[token & tokens] tokens]
     (case token
-      "(" (map read-from-tokens (take-while #(not= ")" %1) tokens))
+      "(" (into [] (map read-from-tokens (take-while #(not= ")" %1) tokens)))
       ")" (throw (Exception. "Unexpected ) while reading"))
-      (atom token))))
+      (atomic token))))
 
-(defn atom)
+(defn parse [string]
+  (read-from-tokens (tokenize string)))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!")
-  (println (tokenize "(+ 1 2)"))
+  (println (str/join "," (tokenize "(+ 1 2)")))
+  (println (parse "(+ 1 2)"))
   )
