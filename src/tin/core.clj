50644c728528d5ada9fe8cadcf2c525060f45685
Derek Thurn <derek@fake.email>
2016-06-20 10:50:41 -0700

    instaparse

--- src/tin/core.clj
+++ src/tin/core.clj
@@ -1,33 +1,16 @@
 (ns tin.core
-  (:require [clojure.string :as str])
+  (:require [clojure.string :as str]
+            [instaparse.core :as insta])
   (:gen-class))
 
-(defn tokenize [string]
-  (filter #(not= %1 "")
-          (str/split
-           (str/replace
-            (str/replace string "(" " ( ") ")" " ) ")
-           #" ")))
-
-(defn atomic [token] {:token token})
-
-(defn read-from-tokens [tokens]
-  ;(println "read-from-tokens>" tokens "<")
-  (if (zero? (count tokens))
-    (throw (Exception. "Unexpected EOF while reading")))
-  (let [[token & tokens] tokens]
-    (case token
-      "(" (into [] (map read-from-tokens (take-while #(not= ")" %1) tokens)))
-      ")" (throw (Exception. "Unexpected ) while reading"))
-      (atomic token))))
-
-(defn parse [string]
-  (read-from-tokens (tokenize string)))
+(def parse
+  (insta/parser
+   "number = #'[0-9]+'"))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!")
-  (println (str/join "," (tokenize "(+ 1 2)")))
-  (println (parse "(+ 1 (+ 2 3))"))
+  (def input "12")
+  (println (parse input))
   )
