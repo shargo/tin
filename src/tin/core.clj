35ea68f4b41e8a075c01be4128221d950a530a44
Derek Thurn <derek@fake.email>
2016-06-23 10:51:03 -0700

    nested expression support

--- src/tin/core.clj
+++ src/tin/core.clj
@@ -5,18 +5,10 @@
 
 (def parse (insta/parser (slurp "src/tin/grammar.ebnf")))
 
-
-(defn evaluate
-  [input]
-  (case (first input)
-    :number (read-string (second input))
-    :symbol (second input)
-    :expression (map evaluate (rest input))))
-
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (def input "(+ 1 2)")
+  (def input "(+ 1 (+ 2 3))")
   (def parsed (parse input))
   (println parsed)
   ;(println (evaluate parsed))
