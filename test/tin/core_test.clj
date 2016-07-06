e310226f8c3221b51c7f693bd7a5fa92dd2e7a9e
Derek Thurn <derek@fake.email>
2016-07-06 10:53:12 -0700

    parsing tests

--- test/tin/core_test.clj
+++ test/tin/core_test.clj
@@ -3,6 +3,17 @@
             [tin.core :refer :all]
             [clojure.string :as str]))
 
+(deftest test-parsing
+  (is (=
+       [:program [:expression [:symbol "a"] [:symbol "="] [:number "1"]]]
+       (parse "(a = 1)")
+       ))
+  (is (=
+       [:program [:expression [:symbol "+"] [:number "1"] [:number "2"]]]
+       (parse "(+ 1 2)")
+       ))
+  )
+
 (deftest test-assignment
   (is (= (str/trim
           (slurp "test/tin/assignment.js"))
