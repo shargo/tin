7d6303cbdb22aa949118be49e59f5fe86fb4afec
Derek Thurn <derek@fake.email>
2016-07-25 09:34:53 -0700

    update tests

--- test/tin/core_test.clj
+++ test/tin/core_test.clj
@@ -9,84 +9,141 @@
    (=
     (parse "let a = 1")
     [:program
-     [:fncall
-      [:function_expression [:symbol "let"]]
-      [:function_arguments [:symbol "a"] [:symbol "="] [:number "1"]]]])))
+     [:statement
+      [:head
+       [:SYMBOL "let"]]
+      [:statement_args
+       [:SYMBOL "a"]
+       [:OPERATOR "="]
+       [:NUMBER "1"]]]])))
 
 (deftest callFn
   (is
    (=
     (parse "foo(bar)")
-    [:program [:fncall [:function_expression [:symbol "foo"]]
-               [:function_arguments [:symbol "bar"]]]])))
+    [:program
+     [:statement
+      [:fncall
+       [:SYMBOL "foo"]
+       [:arglist
+        [:SYMBOL "bar"]]]]])))
 
 (deftest assignCallFn
   (is
    (=
     (parse "let a = foo(bar)")
-    [:program [:fncall [:function_expression [:symbol "let"]]
-               [:function_arguments [:symbol "a"] [:symbol "="]]]
-     [:fncall [:function_expression [:symbol "foo"]]
-      [:function_arguments [:symbol "bar"]]]])))
+    [:program
+     [:statement
+      [:head
+       [:SYMBOL "let"]]
+      [:statement_args
+       [:SYMBOL "a"]
+       [:OPERATOR "="]
+       [:fncall
+        [:SYMBOL "foo"]
+        [:arglist
+         [:SYMBOL "bar"]]]]]])))
 
 (deftest simpleBlock
   (is
    (=
     (parse "a b\n  c d")
     [:program
-     [:fncall
-      [:function_expression [:symbol "a"]]
-      [:function_arguments
-       [:symbol "b"]
-       [:block [:fncall
-                [:function_expression [:symbol "c"]]
-                [:function_arguments [:symbol "d"]]]]]]])))
+     [:statement
+      [:head
+       [:SYMBOL "a"]]
+      [:statement_args
+       [:SYMBOL "b"]
+       [:block
+        [:statement
+         [:head
+          [:SYMBOL "c"]]
+         [:statement_args
+          [:SYMBOL "d"]]]]]]])))
 
 (deftest simpleBlockParens
   (is
    (=
     (parse "a b\n  c(d)")
     [:program
-     [:fncall
-      [:function_expression [:symbol "a"]]
-      [:function_arguments
-       [:symbol "b"]
-       [:block [:fncall
-                [:function_expression [:symbol "c"]]
-                [:function_arguments [:symbol "d"]]]]]]])))
+     [:statement
+      [:head
+       [:SYMBOL "a"]]
+      [:statement_args
+       [:SYMBOL "b"]
+       [:block
+        [:statement
+         [:fncall
+          [:SYMBOL "c"]
+          [:arglist
+           [:SYMBOL "d"]]]]]]]])))
 
 (deftest multilineBlock
   (is
    (=
     (parse "a b\n  c(d)\n  e f")
     [:program
-     [:fncall
-      [:function_expression [:symbol "a"]]
-      [:function_arguments
-       [:symbol "b"]
+     [:statement
+      [:head
+       [:SYMBOL "a"]]
+      [:statement_args
+       [:SYMBOL "b"]
        [:block
-        [:fncall
-         [:function_expression [:symbol "c"]]
-         [:function_arguments [:symbol "d"]]]
-        [:fncall
-         [:function_expression [:symbol "e"]]
-         [:function_arguments [:symbol "f"]]]]]]])))
+        [:statement
+         [:fncall
+          [:SYMBOL "c"]
+          [:arglist
+           [:SYMBOL "d"]]]]
+        [:statement
+         [:head
+          [:SYMBOL "e"]]
+         [:statement_args
+          [:SYMBOL "f"]]]]]]])))
 
 (deftest ifStatement
   (is
    (=
     (parse "if size < 0\n  raise ValueError(errorMessage)")
     [:program
-     [:fncall
-      [:function_expression [:symbol "if"]]
-      [:function_arguments [:symbol "size"] [:symbol "<"] [:number "0"]
+     [:statement
+      [:head
+       [:SYMBOL "if"]]
+      [:statement_args
+       [:SYMBOL "size"]
+       [:OPERATOR "<"]
+       [:NUMBER "0"]
        [:block
-        [:fncall
-         [:function_expression [:symbol "raise"]]
-         [:function_arguments
+        [:statement
+         [:head
+          [:SYMBOL "raise"]]
+         [:statement_args
           [:fncall
-           [:function_expression [:symbol "ValueError"]]
-           [:function_arguments [:symbol "errorMessage"]]]]]]]]])))
+           [:SYMBOL "ValueError"]
+           [:arglist
+            [:SYMBOL "errorMessage"]]]]]]]]])))
+
+(deftest ifStatementParens
+  (is
+   (=
+    (parse "if (size < 0)\n  raise ValueError(errorMessage)")
+    [:program
+     [:statement
+      [:head
+       [:SYMBOL "if"]]
+      [:statement_args
+       [:grouping_expression
+        [:SYMBOL "size"]
+        [:OPERATOR "<"]
+        [:NUMBER "0"]]
+       [:block
+        [:statement
+         [:head
+          [:SYMBOL "raise"]]
+         [:statement_args
+          [:fncall
+           [:SYMBOL "ValueError"]
+           [:arglist
+            [:SYMBOL "errorMessage"]]]]]]]]])))
 
 ;; (deftest test-assignment
 ;;   (is (= (str/trim
