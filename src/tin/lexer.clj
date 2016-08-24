3f1c0b00346b43bc7bf4828f5304ec6e43be79af
Derek Thurn <derek@fake.email>
2016-08-23 21:29:21 -0700

    tokenize-string function

--- src/tin/lexer.clj
+++ src/tin/lexer.clj
@@ -4,25 +4,32 @@
   (:gen-class))
 
 (def token-regexes
-  {
-   "SYMBOL" #"^[\p{L}][-+=!$%^&*<>_|\p{L}\p{N}]*",
-   "KEYWORD" #"^[\p{L}]+:"
-   "OPERATOR" #"^[-+=!$%^&*<>_][-+=!$%^&*<>_|\p{L}\p{N}]*"
-   "NUMBER" #"^[\d]+"
-   "DOT" #"^\."
-   "COMMA" #"^,"
-   "LPAREN" #"^\("
-   "RPAREN" #"^\)"
-   "WS" #"^[ ]+"
-   "NEWLINE" #"\n"
-   })
+  [
+   ["SYMBOL" #"^[\p{L}][-+=!$%^&*<>_|\p{L}\p{N}]*"]
+   ["KEYWORD" #"^[\p{L}]+:"]
+   ["OPERATOR" #"^[-+=!$%^&*<>_][-+=!$%^&*<>_|\p{L}\p{N}]*"]
+   ["NUMBER" #"^[\d]+"]
+   ["DOT" #"^\."]
+   ["COMMA" #"^,"]
+   ["LPAREN" #"^\("]
+   ["RPAREN" #"^\)"]
+   ["WS" #"^[ ]+"]
+   ["NEWLINE" #"^\n"]
+   ])
+
+(defn token-match
+  "Returns a function which accepts a [token regex] pair and returns a [token
+  remainder] pair if the regex matches 'string'."
+  [string]
+  (fn [[name regex]]
+    (when-let [match (re-find regex string)]
+      [name (subs string (count match))])))
 
 (defn next-token
   "Returns a vector pair consisting of a token string representing the first
    token present in 'string' and the remainder of the string"
   [string]
-  []
-  )
+  (some (token-match string) token-regexes))
 
 (defn tokenize-string
   "Performs lexical analysis on string, returning a string containing the
@@ -34,11 +41,11 @@
          is-continued-line? false
          line-number 0
          column-number 0]
-    (empty? rest) result
-    (let [[next-token remainder] (next-token rest)]
-      (recur remainder
-             (conj result next-token)
-             current-indentation
-             is-continued-line?
-             line-number
-             column-number))))
+    (if (empty? rest) result
+        (let [[next-token remainder] (next-token rest)]
+          (recur remainder
+                 (conj result next-token)
+                 current-indentation
+                 is-continued-line?
+                 line-number
+                 column-number)))))
