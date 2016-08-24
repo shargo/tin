55ad6e441eb29401a0606ebe150185ef372a543a
Derek Thurn <derek@fake.email>
2016-08-24 09:37:24 -0700

    ignore whitespace and comments

--- src/tin/lexer.clj
+++ src/tin/lexer.clj
@@ -3,27 +3,38 @@
    [clojure.string :as str])
   (:gen-class))
 
+(def ignored-regex #"(?s)(?:[ ]|/\*.*?\*/)*")
+
+(defn token-regex
+  [regex]
+  (re-pattern (str #"^" regex ignored-regex)))
+
 (def token-regexes
   [
-   ["SYMBOL" #"^[\p{L}][-+=!$%^&*<>_|\p{L}\p{N}]*"]
-   ["KEYWORD" #"^[\p{L}]+:"]
-   ["OPERATOR" #"^[-+=!$%^&*<>_][-+=!$%^&*<>_|\p{L}\p{N}]*"]
-   ["NUMBER" #"^[\d]+"]
-   ["DOT" #"^\."]
-   ["COMMA" #"^,"]
-   ["LPAREN" #"^\("]
-   ["RPAREN" #"^\)"]
-   ["WS" #"^[ ]+"]
-   ["NEWLINE" #"^\n"]
+   ["KEYWORD" #"[\p{L}]+:"]
+   ["SYMBOL" #"[\p{L}][-+=!$%^&*<>_|\p{L}\p{N}]*"]
+   ["OPERATOR" #"[-+=!$%^&*<>_][-+=!$%^&*<>_|\p{L}\p{N}]*"]
+   ["NUMBER" #"[\d]+"]
+   ["DOT" #"\."]
+   ["COMMA" #","]
+   ["LPAREN" #"\("]
+   ["RPAREN" #"\)"]
+   ["LINE_START" #"\n"]
    ])
+
+(defn result-for-match
+  [token-name match]
+  (if (some #{token-name} #{"SYMBOL" "KEYWORD" "OPERATOR" "NUMBER"})
+    (str token-name "(" match ")")
+    token-name))
 
 (defn token-match
   "Returns a function which accepts a [token regex] pair and returns a [token
   remainder] pair if the regex matches 'string'."
   [string]
   (fn [[name regex]]
-    (when-let [match (re-find regex string)]
-      [name (subs string (count match))])))
+    (when-let [match (re-find (token-regex regex) string)]
+      [(result-for-match name match) (subs string (count match))])))
 
 (defn next-token
   "Returns a vector pair consisting of a token string representing the first
@@ -35,10 +46,10 @@
   "Performs lexical analysis on string, returning a string containing the
    tokenized representation of the string or a LexicalError value."
   [string]
-  (loop [rest string
+  (loop [rest (str "\n" string "\n")
          result []
          current-indentation 0
-         is-continued-line? false
+         logical-line? false
          line-number 0
          column-number 0]
     (if (empty? rest) result
@@ -46,6 +57,6 @@
           (recur remainder
                  (conj result next-token)
                  current-indentation
-                 is-continued-line?
+                 logical-line?
                  line-number
                  column-number)))))
